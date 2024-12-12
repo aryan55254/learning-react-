a6656db5c93e20a1bd3231bb13c8b3ef6ba6f956
Aryan <aryanmishra55254@gmail.com>
2024-12-12 23:28:37 +0530

    trying out jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Define the component
+function WelcomeMessage() {
+  const userName = "John";
+  return <h1>Hello, {userName}!</h1>;
+}
+
+// Render the component directly
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WelcomeMessage />
+  </React.StrictMode>
+);
