42b18490a15dce78c307cc01f79bcf8e2e3b9a85
Aryan <aryanmishra55254@gmail.com>
2024-12-20 11:13:04 +0530

    props

--- tailwind-with-react/src/App.jsx
+++ tailwind-with-react/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from "./components/card"
+import Card2 from "./components/card2"
+import Card3 from "./components/card3"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-3xl bg-green-300 p-3">Vite with tailwind </h1>
+    <h1 className="text-3xl bg-green-300 p-3 rounded-md">Vite with tailwind </h1>
+    <Card3 post="staff" />
     </>
   )
 }
---
+++ tailwind-with-react/src/components/card.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function card() {
+  return (
+    
+        <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
+  <img className="w-full h-48 object-cover" src="https://imgs.search.brave.com/PjE33tVn9FgNIvLyiXIda3Iuc-QnbB3f_cexAHHWsHU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/YS1kcm9wLW9mLXBp/bmstYW5kLXllbGxv/dy1wYWludC1pbi13/YXRlci5qcGc_d2lk/dGg9MTAwMCZmb3Jt/YXQ9cGpwZyZleGlm/PTAmaXB0Yz0w" alt="Card image" />
+  <div className="p-4">
+    <h5 className="text-lg font-bold text-gray-800">Card Title</h5>
+    <p className="mt-2 text-sm text-gray-600">
+      This is a simple card component created using Tailwind CSS. Customize it as needed!
+    </p>
+    <div className="mt-4">
+      <a href="#" className="text-indigo-500 font-semibold hover:underline">Learn More</a>
+    </div>
+  </div>
+</div>
+
+
+       
+
+  )
+}
+
+export default card
